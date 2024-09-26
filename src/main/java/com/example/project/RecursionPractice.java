6144646e7886fdcee5b303e8bb615c0b1c59a576
bigears555 <cillian.a.doherty@outlook.com>
2024-09-26 12:31:34 +0000

    turning in my work

--- src/main/java/com/example/project/RecursionPractice.java
+++ src/main/java/com/example/project/RecursionPractice.java
@@ -2,22 +2,35 @@ package com.example.project;
 
 public class RecursionPractice {
 
+	private static final String What = null;
+
 	public static void main(final String[] args) {
 		//use main to test your functions!
+		factorial(3);
+		sum(4);
+		fibonacci(4);
+		findTheFirstW(What, 0);
 	}
 	
 	//factorial of n should return the long n multiplied by all numbers before it. 
 	//if n == 0 or n == 1, 1 should be returned.
 	//note that a long is just an int with twice the memory
-	public static long factorial(long n){
-		return 0;
+	public static long factorial(long n){ 
+		if (n == 0) {   
+			return 1;    
+		} else { 
+			return(n * factorial(n-1)); 
+	}
 	}
 
 	//sum of n should return the long n added  by all numbers before it. 
 	//if n == 0, 0 should be returned.
 	//this is essentially factorial but with addition
 	public static long sum(long n){
-		return 0;
+		if (n == 0)
+			return 1;
+		else
+			return(n + (n-1));
 	}
 	
 	// The fibonacci of n should return the long value at position n in the Fibonacci sequence.
@@ -25,7 +38,13 @@ public class RecursionPractice {
 	// If n == 1, 1 should be returned.
 	// For n > 1, it should return the sum of fibonacci(n - 1) and fibonacci(n - 2).
 	public static long fibonacci(long n){
-		return 0;
+		if (n == 0)
+			return 0;
+		else if (n == 1)
+			return 1;
+		else
+			return fibonacci(n - 1) + fibonacci(n - 2);
+			
 	}
 
 	//Should return the index of the first W/w in a string. 
@@ -39,7 +58,13 @@ public class RecursionPractice {
 	//1)  the character at the current index is W
 	//2) The current index is out of bounds
 	public static int findTheFirstW(String s, int curIndex){
-		return -1;
+		if (curIndex >= s.length()){
+			return -1;
+		}
+		if (s.charAt(curIndex) == 'W' || s.charAt(curIndex) == 'w'){
+			return curIndex;
+		}
+		return findTheFirstW(s, curIndex + 1);
 	}
 }
 
